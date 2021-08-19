0d0b3585cbda606be282dd2738044f1040c4cff2
Stanislav Rakitov <prosperousrf@gmail.com>
2021-08-19 15:36:42 +0300

    Breaking chocolate problem

---
+++ tests/ChocolateTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Stanislav Rakitov
+ */
+public class ChocolateTest {
+
+  @Test
+  public void myTests() {
+    assertEquals(24, Chocolate.breakChocolate(5, 5));
+    assertEquals(0, Chocolate.breakChocolate(1, 1));
+  }
+}
